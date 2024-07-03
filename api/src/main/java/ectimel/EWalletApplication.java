e7e7bb879b41e5173b2eca46f109819103929fa0
Dominik Tworek <dtworek94@gmail.com>
2024-07-03 12:13:17 +0100

    Created api, shared, user_access, wallet modules and achieved basic pom configuration. In addition splited walled module to be reade to adopt clean architecture with event driven approach by creating 4 submodules: domain, application, infrastructure, and integration_events.

--- api/src/main/java/ectimel/EWalletApplication.java
+++ api/src/main/java/ectimel/EWalletApplication.java
@@ -1,4 +1,4 @@
-package ectimel.ewallet;
+package ectimel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ wallet/domain/src/main/java/ectimel/Wallet.java
@@ -0,0 +1,4 @@
+package ectimel;
+
+public class Wallet {
+}
